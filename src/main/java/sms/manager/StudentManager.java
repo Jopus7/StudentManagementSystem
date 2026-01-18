0eab39c6db90118ac9b8942784f8695ca98a4102
Jopus7 <micha.jopa@gmail.com>
2026-01-18 13:42:41 +0100

    added StudentManager interface, added gitignore

---
+++ src/main/java/sms/manager/StudentManager.java
@@ -0,0 +1,17 @@
+package sms.manager;
+
+import sms.model.Student;
+import java.util.ArrayList;
+
+public interface StudentManager {
+
+    void addStudent(Student student);
+
+    void removeStudent(String studentID);
+
+    void updateStudent(String studentID, Student updatedStudent);
+
+    ArrayList<Student> displayAllStudents();
+
+    double calculateAverageGrade();
+}
